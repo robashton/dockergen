f7d045082610525463b8e44981dd979e14165002
Rob Ashton <robashton@codeofrob.com>
2014-06-13 07:46:30 +0100

    All the bits of software

---
+++ examples/elasticsearch.js
@@ -0,0 +1,12 @@
+var dsl = require('../index')
+
+module.exports = dsl()
+                  .wget({
+                    dir: "/root/elasticsearch",
+                    from: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.tar.gz"
+                  })
+                  .tar({
+                    flags: "xzf",
+                    file: "elasticsearch-1.2.1.tar.gz"
+                  })
+                  .run("cp -r /root/elasticsearch/elasticsearch-1.2.1 /opt/elasticsearch")
---
+++ examples/grafana.js
@@ -0,0 +1,12 @@
+var dsl = require('../index')
+
+module.exports = dsl()
+              .wget({
+                dir: "/root/grafana",
+                from: "http://grafanarel.s3.amazonaws.com/grafana-1.5.4.tar.gz"
+              })
+              .tar({
+                flags: "xzf",
+                file: "grafana-1.5.4.tar.gz"
+              })
+              .run("npm install -g http-server")
