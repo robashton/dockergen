849e8c9446cc14502c280c6ad723f972ee97c4a1
Rob Ashton <robashton@codeofrob.com>
2014-06-28 16:57:17 +0200

    Temporary name for publish

--- test.js
+++
@@ -1,8 +0,0 @@
-var blah = require('./index')
-  , fs = require('fs')
-  , path = require('path')
-  , util = require('util')
-  , derp = require('mkdirp')
-
-var box = require('./examples/dashbox')
-
