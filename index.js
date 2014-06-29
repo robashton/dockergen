c8ad812e1a4e75504b6689647efc9016a351a481
Rob Ashton <robashton@codeofrob.com>
2014-06-29 18:16:20 +0200

    Allow port exposing via CLI run-tool

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
   var Step = require('./lib/step')
 
-module.exports = function(name) {
+var mod = function(name) {
   return new Step().step(function(ctx) {
     ctx.instruction("\n###- " + name + " -###\n")
   })
+}
+
+var box = function(name) {
+  return new Step().step(function(ctx) {
+    ctx.packageName(name)
+  })
+}
+
+module.exports = {
+  module: mod,
+  box: box,
+  mustache: require('./lib/mustache')
 }
